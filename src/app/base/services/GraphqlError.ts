111c6f8e5214001b2a3d97d4ad5e6d97a844411f
Richard Quenta <richi617@gmail.com>
2025-01-24 16:44:55 -0400

    update: actualizacion critica graphqlerror

--- src/app/base/services/GraphqlError.ts
+++ src/app/base/services/GraphqlError.ts
@@ -1,21 +1,31 @@
 const validateGraphQlError = (e: Error): any => {
   try {
     const parsed = JSON.parse(JSON.stringify(e, undefined, 2))
+    if (parsed.response.status === 404) {
+      return {
+        status: 404,
+        message: 'Error conexión con el servidor',
+        originalMessage: 'Error conexión con el servidor',
+        stacktrace: `<pre></pre>`,
+        type: 'BAD_REQUEST',
+        path: 'fontEnd',
+      }
+    }
+
     return {
-      status: parsed.response?.errors[0]?.extensions.exception.status || 400,
+      status: parsed.response?.errors[0]?.extensions?.status || 400,
       message: parsed.response?.errors[0]?.message || 'Error no definido',
-      originalMessage:
-        parsed.response?.errors[0]?.extensions.exception.originalMessage || '',
+      originalMessage: parsed.response?.errors[0]?.extensions?.originalMessage || '',
       stacktrace: `<code>${
-        parsed.response?.errors[0]?.extensions?.exception?.stacktrace?.join('') || ''
+        parsed.response?.errors[0]?.extensions?.stacktrace?.join('') || ''
       }</code>`,
-      type: parsed.response?.errors[0]?.extensions?.exception?.type || 'BAD_REQUEST',
-      path: parsed.response?.errors[0]?.path || '',
+      type: parsed.response?.errors[0]?.extensions?.code || 'BAD_REQUEST',
+      path: parsed.response?.errors[0]?.path.join('<br />') || '',
     }
   } catch (e: any) {
     return {
       status: 400,
-      message: e.message,
+      message: 'Error no definido',
       originalMessage: e.message,
       stacktrace: `<pre></pre>`,
       type: 'BAD_REQUEST',
@@ -31,6 +41,7 @@ export class MyGraphQlError extends Error {
   constructor(e: Error) {
     const errors = validateGraphQlError(e)
     super(errors.message) // (1)
+    console.log(import.meta.env.DEV, 'env')
     this.name = `${errors.status} ${errors.type} (${errors.path})` // (2)
     this.stack = import.meta.env.DEV ? errors.stacktrace : ''
     this.cause = import.meta.env.DEV ? errors.originalMessage : ''
