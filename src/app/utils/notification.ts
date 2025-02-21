be84717d683cea60218df86a41ac5365016dfe66
Richard Quenta <richi617@gmail.com>
2025-02-20 23:14:48 -0400

    update: toas msg

--- src/app/utils/notification.ts
+++ src/app/utils/notification.ts
@@ -1,25 +1,38 @@
-import { toast, ToastOptions } from 'react-toastify'
+import { toast, ToastOptions } from "react-toastify";
 
 const options: ToastOptions = {
-  position: 'top-center',
-  theme: 'colored',
-}
+  position: "top-center",
+  theme: "colored"
+};
 
 export const notError = (msg: string) => {
-  toast.error(msg, { ...options })
-}
+  toast.error(msg, { ...options });
+};
 
 /**
  * @description Notificacion de accion satisfactoria
  * @param msg
  */
-export const notSuccess = (msg: string = 'Acción realizada correctamente') => {
-  toast.success(msg, { ...options })
-}
+export const notSuccess = (msg: string = "Acción realizada correctamente") => {
+  toast.success(msg, { ...options });
+};
 /**
  * @description Notificacion de de alerta
  * @param msg
  */
 export const notDanger = (msg: string) => {
-  toast.warning(msg, { ...options })
-}
+  toast.warning(msg, { ...options });
+};
+
+/**
+ * Mensaje generico
+ * @param msg
+ * @param opts
+ */
+export const toastMsg = (msg: string, opts?: ToastOptions) => {
+  toast(msg, {
+    ...options,
+    theme: "light",
+    ...(opts ?? {})
+  });
+};
