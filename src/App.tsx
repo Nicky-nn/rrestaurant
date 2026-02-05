6edcf9ae9ac943bcb2f32888d491251c9dc1c5b4
Richard Quenta <richi617@gmail.com>
2026-02-05 17:52:56 -0400

    feat: 2026.2.16 202602051748 agregado appconfirm

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { useRoutes } from 'react-router-dom'
 
 import ReloadPrompt from './app/base/components/ReloadPrompt/ReloadPrompt'
 import MatxTheme from './app/base/components/Template/MatxTheme/MatxTheme'
+import { AppConfirmProvider } from './app/base/contexts/AppConfirmProvider.tsx'
 import { BreadcrumbProvider } from './app/base/contexts/BreadcrumbContext'
 import { AuthProvider } from './app/base/contexts/JWTAuthContext'
 import { SecurityProvider } from './app/base/contexts/SecurityContext'
@@ -36,56 +37,67 @@ function App() {
         <BreadcrumbProvider>
           <SecurityProvider>
             <MatxTheme>
-              <ConfirmProvider
+              <AppConfirmProvider
                 defaultOptions={{
-                  title: 'Confirmación',
-                  content: '¿Confirma que desea realizar la acción?',
-                  allowClose: false,
-                  confirmationText: 'Confirmar',
+                  title: 'Autorización',
+                  content: '¿Proceder con la solicitud?',
+                  confirmationText: 'Aceptar',
                   cancellationText: 'Cancelar',
-                  dialogProps: {
-                    fullWidth: true,
-                    maxWidth: 'xs',
-                    disableRestoreFocus: true,
-                    disableEnforceFocus: true,
-                    'aria-hidden': false,
-                  },
-                  titleProps: {
-                    sx: {
-                      py: 1.5,
-                    },
-                  },
-                  contentProps: {
-                    dividers: true,
-                    sx: {
-                      bgcolor: 'background.paper',
-                    },
-                  },
-                  dialogActionsProps: {
-                    sx: { justifyContent: 'center' },
-                  },
-                  confirmationButtonProps: {
-                    color: 'primary',
-                    variant: 'contained',
-                    size: 'medium',
-                    startIcon: <CheckCircleTwoTone />,
-                    'aria-label': 'confirm',
-                    autoFocus: false,
-                  },
-                  cancellationButtonProps: {
-                    color: 'error',
-                    variant: 'text',
-                    size: 'medium',
-                    'aria-label': 'close',
-                    autoFocus: false,
-                  },
-                  buttonOrder: ['cancel', 'confirm'],
+                  showInput: false,
+                  disableCloseOnOverlay: false,
                 }}
               >
-                <CssBaseline />
-                {content}
-                <ReloadPrompt />
-              </ConfirmProvider>
+                <ConfirmProvider
+                  defaultOptions={{
+                    title: 'Confirmación',
+                    content: '¿Confirma que desea realizar la acción?',
+                    allowClose: false,
+                    confirmationText: 'Confirmar',
+                    cancellationText: 'Cancelar',
+                    dialogProps: {
+                      fullWidth: true,
+                      maxWidth: 'xs',
+                      disableRestoreFocus: true,
+                      disableEnforceFocus: true,
+                      'aria-hidden': false,
+                    },
+                    titleProps: {
+                      sx: {
+                        py: 1.5,
+                      },
+                    },
+                    contentProps: {
+                      dividers: true,
+                      sx: {
+                        bgcolor: 'background.paper',
+                      },
+                    },
+                    dialogActionsProps: {
+                      sx: { justifyContent: 'center' },
+                    },
+                    confirmationButtonProps: {
+                      color: 'primary',
+                      variant: 'contained',
+                      size: 'medium',
+                      startIcon: <CheckCircleTwoTone />,
+                      'aria-label': 'confirm',
+                      autoFocus: false,
+                    },
+                    cancellationButtonProps: {
+                      color: 'error',
+                      variant: 'text',
+                      size: 'medium',
+                      'aria-label': 'close',
+                      autoFocus: false,
+                    },
+                    buttonOrder: ['cancel', 'confirm'],
+                  }}
+                >
+                  <CssBaseline />
+                  {content}
+                  <ReloadPrompt />
+                </ConfirmProvider>
+              </AppConfirmProvider>
             </MatxTheme>
           </SecurityProvider>
         </BreadcrumbProvider>
